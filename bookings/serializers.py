51c2722e4a7c5142a0703ac8f2b8ac3c36f32de5
mozadev <ceosmore@gmail.com>
2024-10-17 21:17:14 -0500

    Implementation of endpoint nested to appointment

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -3,12 +3,13 @@ from .models import Appointment, MedicalNote
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Appointment
-        fields = '__all__'
+        fields = "__all__"
 
 
 class MedicalNoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = MedicalNote
-        fields = '__all__'
+        fields = "__all__"
