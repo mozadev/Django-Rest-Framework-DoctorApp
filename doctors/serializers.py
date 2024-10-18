ea318bc7ab85b451983cf09e7bcf4fa32614d37b
mozadev <ceosmore@gmail.com>
2024-10-17 19:25:40 -0500

    Implementing serializer nested in doctors to show appointments

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -1,20 +1,30 @@
 from rest_framework import serializers
 
 from .models import Doctor, Department, DoctorAvailability, MedicalNote
+from bookings.serializers import AppointmentSerializer
 
 
 class DoctorSerializer(serializers.ModelSerializer):
+    appointments = AppointmentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Doctor
-        fields = "__all__"
+        fields = [
+            "first_name",
+            "last_name",
+            "qualification",
+            "contact_number",
+            "email",
+            "address",
+            "biography",
+            "is_on_vacation",
+            "appointments",
+        ]
 
     def validate_email(self, value):
         if "@example.com" in value:
             return value
-        raise serializers.ValidationError(
-            "The email have to include the @example.com"
-            )
+        raise serializers.ValidationError("The email have to include the @example.com")
 
     def validate(self, attrs):
         if len(attrs["contact_number"]) < 10 and attrs["is_on_vacation"]:
@@ -22,6 +32,7 @@ class DoctorSerializer(serializers.ModelSerializer):
                 "Please, enter a number before you go on vacations"
             )
         return super().validate(attrs)
+
     # if not continue with functionality of father class
 
 
